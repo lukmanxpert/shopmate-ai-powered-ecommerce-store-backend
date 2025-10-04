664a92f394a1ef4b66c2ad5ce7ecb9b4e08259c7
lukmanxpert <lukmanmiah2004@gmail.com>
2025-10-05 03:36:17 +0600

    added admin routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import { createTables } from "./utils/createTables.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import authRouter from "./router/authRoutes.js";
 import productRouter from "./router/productRoutes.js";
+import adminRouter from "./router/adminRoutes.js";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -35,6 +36,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
+app.use("/api/v1/admin", adminRouter);
 
 createTables();
 app.use(errorMiddleware);
---
+++ controller/adminController.js
@@ -0,0 +1,4 @@
+import ErrorHandler from "../middlewares/errorMiddleware.js";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import database from "../database/db.js";
+
