2a64ed9776cf62d2e72b9dc18122d0b0d8a68f7c
lukmanxpert <lukmanmiah2004@gmail.com>
2025-10-05 04:11:04 +0600

    added get all users api

--- router/adminRoutes.js
+++ router/adminRoutes.js
@@ -1,5 +1,13 @@
 import express from "express";
+import { authorizedRoles, isAuthenticated } from "../middlewares/authMiddleware.js";
+import { getAllUsers } from "../controller/adminController.js";
 
 const router = express.Router();
+router.get(
+  "/getallusers",
+  isAuthenticated,
+  authorizedRoles("Admin"),
+  getAllUsers
+); // DASHBOARD
 
 export default router;
